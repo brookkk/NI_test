e1fa42867a0b65b6ea400136a68e2027873968bc
GRINI <abdellah.grini@sofrecom.com>
2019-08-11 14:46:38 +0100

    generating product entity

---
+++ src/NI/PlatformBundle/Entity/product.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace NI\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="NI\PlatformBundle\Repository\productRepository")
+ */
+class product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sku", type="string", length=255)
+     */
+    private $sku;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set sku
+     *
+     * @param string $sku
+     *
+     * @return product
+     */
+    public function setSku($sku)
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    /**
+     * Get sku
+     *
+     * @return string
+     */
+    public function getSku()
+    {
+        return $this->sku;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
